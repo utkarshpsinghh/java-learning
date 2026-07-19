fe201e271de2e9c3342c3d9b0c90812664cb4c08
Utkarsh Pratap Singh <utkarshetw2006@gmail.com>
2026-07-19 20:25:07 +0530

    Day 2 - Data Types, Variables and Operators

---
+++ Day-02/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Utkarsh";
+        double weight = 55.6;
+        int age = 20;
+        char gender = 'M';
+        boolean isAdult = true;
+
+        System.out.println("Name : " + name);
+        System.out.println("Weight : " + weight);
+        System.out.println("Age : " + age);
+        System.out.println("Gender : " + gender);
+        System.out.println("Adult : " + isAdult);
+
+        double a = 20;
+        double b = 10;
+        System.out.println(a+b);
+
+
+    }
+}
