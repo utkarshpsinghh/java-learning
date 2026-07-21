2c20ad7394b482edd89517a1a7b59eedc3097586
Utkarsh Pratap Singh <utkarshetw2006@gmail.com>
2026-07-22 04:06:57 +0530

    Day - 06 Printf

---
+++ Day-06/Main.java
@@ -0,0 +1,15 @@
+public class Main  {
+    public static  void main (String[] args) {
+
+        String name = "Utkarsh";
+        int age = 20;
+        double weight = 56.6;
+        char gender = 'M';
+
+        System.out.printf("\n%s", name);
+        System.out.printf("\n%d", age);
+        System.out.printf("\n%.1f", weight);
+        System.out.printf("\n%c", gender);
+
+    }
+ }
