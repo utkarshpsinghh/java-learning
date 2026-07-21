dcdcba092504237caf4f11aa96447d92fc3c81ca
Utkarsh Pratap Singh <utkarshetw2006@gmail.com>
2026-07-22 03:17:43 +0530

    Day - 04 Random Numbers in Java

---
+++ Day-04/Main.java
@@ -0,0 +1,27 @@
+package day04;
+import  java.util.Random;
+public class Main{
+    public static void main(String[] args) {
+        Random rn = new Random();
+
+        int number;
+        boolean isHead;
+        double number1;
+
+        number = rn.nextInt(1, 7);
+        System.out.println(number);
+        number1 = rn.nextDouble(1, 7);
+        System.out.println(number1);
+
+
+        isHead = rn.nextBoolean();
+
+        if (isHead) {
+            System.out.println("Heads");
+        } else {
+            System.out.println("Tails");
+
+        }
+
+    }
+}
